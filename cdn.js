35ede3ff7826af56bec4e058ff4f4318330e722e
okappes <okappes@amadeus.com>
2013-09-16 17:51:41 +0200

    Handling of dev builds + external config

--- cdn.js
+++ cdn.js
@@ -1,14 +1,16 @@
 var SERVER_PORT = 5001;
-var PATH_TO_MIN_FWK = __dirname + '/aria/';
-var CDN_URL = 'http://cdn.ariatemplates.com';
 
+var config = require('./cdn.conf');
 var express = require('express');
 var fs = require('fs');
 
 var app = express();
 
-var fwkcache = {};
+var fwkdevcache = {}, fwkcache = {};
 
+/*
+ * Middleware to allow cross-domain requests
+ */
 var allowCrossDomain = function (req, res, next) {
 	res.header('Access-Control-Allow-Origin', "*");
 	res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
@@ -21,6 +23,9 @@ var allowCrossDomain = function (req, res, next) {
 	}
 }
 
+/*
+ * Middleware to lowercase the querystring
+ */
 var lowerCaseQuery = function (req, res, next) {
 	var newq = {};
 	for (var key in req.query) newq[key.toLowerCase()] = req.query[key];
@@ -29,75 +34,164 @@ var lowerCaseQuery = function (req, res, next) {
 }
 
 app.configure(function() {
-    app.use(allowCrossDomain);
+	app.use(allowCrossDomain);
 	app.use(lowerCaseQuery);
-    app.use('/aria', express.static(__dirname + '/aria'));
-    app.use(express.static(__dirname + '/public'));
+	app.use('/aria', express.static(__dirname + '/aria'));
+	app.use('/dev', express.static(__dirname + '/dev'));
+	app.use(express.static(__dirname + '/public'));
 });
 
-// TODO: handle min|dev - dev files cannot obviously reside in the same folder
+/*
+ * Open-source build getter
+ *
+ * /ariatemplates-<x.y.z>.js[?<params>]
+ *
+ * params:
+ * - dev  (dev build, default to minified)
+ * - skin (serve skin, default does not)
+ */
+app.get(/^\/ariatemplates-(\d\.\d\.\d{1,2})\.js/, function (req, res) {
+	getFwk(res, 'ariatemplates-', req.params[0], req.query.dev != undefined, req.query.skin != undefined);
+});
 
-// URL pattern matches /at<version>.js[?<params>]
-// with params: 1a (Amadeus package, default to OS one), dev (dev build, default to minified), skin (serve skin, default does not)
+/*
+ * Amadeus build getter
+ *
+ * /aria-tamplates-<x.y-z[p]>.js[?<params>]
+ *
+ * params:
+ * - dev  (dev build, default to minified)
+ * - skin (serve skin, default does not)
+ */
+app.get(/^\/aria-templates-(\d\.\d\-\d{1,2}[a-zA-Z]?)\.js/, function (req, res) {
+	getFwk(res, 'aria-templates-', req.params[0], req.query.dev != undefined, req.query.skin != undefined);
+});
 
-app.get(/^\/at(\d)[\-\.]?(\d)[\-\.]?(\d{1,2})([a-zA-Z]?).js/, function (req, res) {
+/*
+ * Generic getter
+ *
+ * /at<version>.js[?<params>]
+ *
+ * params:
+ * - 1a   (Amadeus package, default to OS one)
+ * - dev  (dev build, default to minified)
+ * - skin (serve skin, default does not)
+ */
+app.get(/^\/at(\d)[\-\.]?(\d)[\-\.]?(\d{1,2})([a-zA-Z]?)\.js/, function (req, res) {
 	var amadeus = req.query['1a'] != undefined;
-	var skin = req.query.skin != undefined;
+	getFwk(
+		res, // res
+		amadeus ? 'aria-templates-' : 'ariatemplates-', // suffix
+		req.params[0] + '.' + req.params[1] + (amadeus ? '-' : '.') + req.params[2] + req.params[3], // version
+		req.query.dev != undefined, // dev?
+		req.query.skin != undefined // skin?
+	);
+});
 
-	var version = req.params[0] + '.' + req.params[1];
-	version += amadeus ? '-' : '.';
-	version += req.params[2] + req.params[3]; // shouldn't be no patched OS version though
+/*
+ * Shortcut to the generic getter for the latest version (same params)
+ */
+app.get('/atlatest.js', function (req, res) {
+	var amadeus = req.query['1a'] != undefined;
+	getFwk(
+		res, // res
+		amadeus ? 'aria-templates-' : 'ariatemplates-', // suffix
+		config.LATEST.substr(0,1) + '.' + config.LATEST.substr(1,1) + (amadeus ? '-' : '.') + config.LATEST.substr(2), // version
+		req.query.dev != undefined, // dev?
+		req.query.skin != undefined // skin?
+	);
+})
 
-	var filename = (amadeus ? 'aria-templates-' : 'ariatemplates-')	+ version + '.js';
+/*
+ * Retrieve the content of the bootstrap file from disk or cache
+ */
+var getFwk = function (res, suffix, version, dev, skin) {
+	var cache = dev ? fwkdevcache : fwkcache;
+	var filename = suffix + version + '.js';
 
-	var result = '';
-	if (fwkcache[filename]) {
+	if (cache[filename]) {
+		debugger
 		console.log('using cache for ' + filename);
-		sendFwk(res, fwkcache[filename], version, skin)
+		sendFwk(res, cache[filename], version, dev, skin)
 	} else {
-		console.log('looking for ' + filename);
-		fs.exists(PATH_TO_MIN_FWK + filename, function (exists) {
+		var fwkfile = (dev ? config.PATH_TO_DEV_FWK + version + '/aria/' : config.PATH_TO_MIN_FWK) + filename;
+		console.log('looking for ' + fwkfile);
+		fs.exists(fwkfile, function (exists) {
 			if (exists) {
-				fs.readFile(PATH_TO_MIN_FWK + filename, function (err, data) {
+				fs.readFile(fwkfile, function (err, data) {
 					if (err) {
-						console.log('an error occured trying to read ' + filename);
+						console.log('an error occured trying to read ' + fwkfile);
 						throw err;
 					}
 					else {
-						fwkcache[filename] = data;
-						sendFwk(res, data, version, skin);
+						cache[filename] = data;
+						sendFwk(res, data, version, dev, skin);
 					}
 				});
 			}
 			else {
-				console.log('file ' + PATH_TO_MIN_FWK + filename + ' not found');
+				console.log('file ' + fwkfile + ' not found');
 				res.send(404);
 			}
 		});		
 	}
-});
+};
 
-var sendFwk = function (res, content, version, skin) {
+/*
+ * Send the content of the bootstrap along with the necessary JS lines to add skin, change dev url, update transport, update the root map.
+ */
+var sendFwk = function (res, content, version, dev, skin) {
 	var l = content.length;
+	var url = config.CDN_URL + (dev ? '/dev/' + version : '');
+
+	// prepare buffers
+	if (dev) {
+		console.log('using dev')
+		var bufDev = new Buffer('document.write("<script>___baseURL___=\'' + url + '/\'</script>");', 'utf-8');
+		l += bufDev.length;
+	}
 	if (skin) {
 		console.log('using skin')
-		var bufSkin = new Buffer('document.write(\'<script src="'+ CDN_URL + '/aria/css/atskin-' + version + '.js"><\/script>\');', 'utf-8');
+		var bufSkin = new Buffer('document.write(\'<script src="'+ url + '/aria/css/atskin-' + version + '.js"><\/script>\');', 'utf-8');
 		l += bufSkin.length;
 	}
-	var bufFix = new Buffer('document.write("<script>aria.core.IO.updateTransports({\'crossDomain\' : \'aria.core.transport.XHR\'});aria.core.DownloadMgr.updateRootMap({\'aria\' : \'' + CDN_URL + '/\',	\'*\' : \'\'});</script>");', 'utf-8');
+	var bufFix = new Buffer('document.write("<script>aria.core.IO.updateTransports({\'crossDomain\' : \'aria.core.transport.XHR\'});aria.core.DownloadMgr.updateRootMap({\'aria\' : \'' + url + '/\',	\'*\' : \'\'});</script>");', 'utf-8');
 	l += bufFix.length;
-	var r = new Buffer(l);
-	
-	content.copy(r);
-	if (skin) {
-		bufSkin.copy(r, content.length);
-		bufFix.copy(r, content.length + bufSkin.length);
-	} else {
-		bufFix.copy(r, content.length);
-	}
 
+	var r = new Buffer(l), offset = 0;
+
+	// fill response
+	if (dev) {
+		bufDev.copy(r, offset);
+		offset += bufDev.length;
+	}
+	content.copy(r, offset);
+	offset += content.length;
+	if (skin) {
+		bufSkin.copy(r, offset);
+		offset += bufSkin.length;
+	}
+	bufFix.copy(r, offset);
+
+	res.header('Content-Type', 'application/javascript');
 	res.send(r);
 }
+
+/*
+ * URL to use to reload the config without restarting the server
+ */
+app.get('/updateconfig', function (req, res) {
+	if (req.ip == '127.0.0.1') {
+		delete require.cache[require.resolve('./cdn.conf')];
+		config = require('./cdn.conf');
+		console.log('Configuration reloaded');
+		res.send(200);
+	} else {
+		console.log('Remote attempt at updating the configuration from ' + req.ip);
+		res.send(404);
+	}
+});
+
 
 app.listen(SERVER_PORT);
 console.log('CDN started on port ' + SERVER_PORT);

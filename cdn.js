5f935d61cd731aa3fa5c7042a4ef78a0f6efefdf
okappes <okappes@amadeus.com>
2013-11-15 11:19:52 +0100

    Cache headers + /versions

--- cdn.js
+++ cdn.js
@@ -1,12 +1,19 @@
 var SERVER_PORT = 5001;
 
-var config = require('./cdn.conf');
+var ONE_YEAR = 31536000; // one year in s
+var ONE_YEAR_MS = ONE_YEAR * 1000; // one year in ms
+
+var CONF_FILE = '/srv/www/cdn.ariatemplates.com/cdn.conf';
+
+var config = require(CONF_FILE);
 var express = require('express');
 var fs = require('fs');
 
 var app = express();
 
 var fwkdevcache = {}, fwkcache = {};
+
+var LATEST_TS = fs.statSync(CONF_FILE).mtime.toUTCString();
 
 /*
  * Middleware to allow cross-domain requests
@@ -46,12 +53,13 @@ var normalizePaths = function(paths) {
  * App environments configuration
  */
 app.configure(function() {
+	app.use(express.compress()); // gzip
 	app.use(allowCrossDomain);
 	app.use(lowerCaseQuery);
-	app.use('/aria', express.static(__dirname + '/aria'));
-	app.use('/dev', express.static(__dirname + '/dev'));
-	app.use('/css', express.static(__dirname + '/css'));
-	app.use(express.static(__dirname + '/static'));
+	app.use('/aria', express.static(__dirname + '/aria', {maxAge: ONE_YEAR_MS}));
+	app.use('/dev', express.static(__dirname + '/dev', {maxAge: ONE_YEAR_MS}));
+	app.use('/css', express.static(__dirname + '/css', {maxAge: ONE_YEAR_MS}));
+	app.use(express.static(__dirname + '/static', {maxAge: ONE_YEAR_MS}));
 	normalizePaths(config.path);
 });
 
@@ -106,6 +114,7 @@ app.get(/^\/at(\d)[\-\.]?(\d)[\-\.]?(\d{1,2})([a-zA-Z]?)\.js/, function (req, re
  */
 app.get('/atlatest.js', function (req, res) {
 	var amadeus = req.query['1a'] != undefined;
+	res.setHeader('Last-Modified', LATEST_TS);
 	getFwk(
 		req,
 		res,
@@ -196,6 +205,8 @@ var sendFwk = function (res, content, version, dev, skin, root) {
 	bufFix.copy(r, offset);
 
 	res.header('Content-Type', 'application/javascript');
+	res.header('Cache-Control', 'public, max-age=' + ONE_YEAR);
+
 	res.send(r);
 }
 
@@ -204,9 +215,10 @@ var sendFwk = function (res, content, version, dev, skin, root) {
  */
 app.get('/updateconfig', function (req, res) {
 	if (req.ip == '127.0.0.1') {
-		delete require.cache[require.resolve('./cdn.conf')];
-		config = require('./cdn.conf');
+		delete require.cache[require.resolve(CONF_FILE)];
+		config = require(CONF_FILE);
 		normalizePaths(config.path);
+		LATEST_TS = (new Date()).toUTCString();
 		console.log('Configuration reloaded');
 		res.send(200);
 	} else {
@@ -214,6 +226,17 @@ app.get('/updateconfig', function (req, res) {
 		res.send(404);
 	}
 });
+
+app.get('/versions', function (req, res) {
+	var versions = {
+		min : config.OLDEST,
+		max : config.LATEST
+	};
+	res.header('Content-Type', 'application/json');
+	res.header('Cache-Control', 'public, max-age=' + ONE_YEAR);
+	res.setHeader('Last-Modified', LATEST_TS);
+	res.send(versions);
+})
 
 /*
  * Anything that hasn't been routed at this point is a 404
